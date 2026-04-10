1c30df356aebdc1b95b3fd5cb461cf09697c79a1
Rafael Rodrigues <rafaelrlimaa@gmail.com>
2026-04-10 03:24:31 -0300

    finalizando esse modulo

--- src/app/lista-comrpas/lista-comrpas.component.ts
+++ src/app/lista-comrpas/lista-comrpas.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import {ItemLista} from './itemlista';
+import {CommonModule} from '@angular/common'
 
 @Component({
   selector: 'app-lista-comrpas',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-comrpas.component.html',
   styleUrl: './lista-comrpas.component.scss'
 })
 export class ListaComrpasComponent {
   item: String = '';
+  lista: ItemLista[] = [];
 
   adicionarItem(){
-    console.log("Item recebido: ", this.item );
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
     this.item = '';
+  }
+  riscarItem(itemLista: ItemLista){
+    itemLista.comprado = !itemLista.comprado
+  }
+
+  limparLista(){
+    this.lista = [];
   }
 }
